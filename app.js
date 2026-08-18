e57065eaaad23434093ddd60c3e41a7a8e3bc1e2
telkisd-cmd <telkisd@gmail.com>
2026-08-18 22:54:08 +0300

    Update app.js

--- app.js
+++ app.js
@@ -81,8 +81,6 @@ document.getElementById("calculatorTab").style.display =
   if(tab==="paint")
     document.getElementById("paintTab").style.display =
       "block";
-  
-  alert("Paint opened");
 
   if(tab==="hull")
     document.getElementById("hullTab").style.display =
