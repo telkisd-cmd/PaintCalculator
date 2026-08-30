955fc05bdf1292e936bab8bd7360482a48384a50
telkisd-cmd <telkisd@gmail.com>
2026-08-30 20:38:10 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,48 @@
-self.addEventListener("install", (event) => {
+const CACHE_NAME = "paintcalc-v1";
+
+const urlsToCache = [
+  "/PaintCalculator/",
+  "/PaintCalculator/index.html",
+  "/PaintCalculator/style.css",
+  "/PaintCalculator/app.js",
+  "/PaintCalculator/manifest.json",
+  "/PaintCalculator/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+
+  event.waitUntil(
+
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+
+  );
+
   self.skipWaiting();
+
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request));
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+    self.clients.claim()
+  );
+
+});
+
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+
+    caches.match(event.request)
+      .then(response => {
+
+        return response || fetch(event.request);
+
+      })
+
+  );
+
 });
