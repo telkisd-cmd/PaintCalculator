c7887fb24618974671e1f2867bb065ebfad2771d
telkisd-cmd <telkisd@gmail.com>
2026-08-31 12:59:04 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -37,12 +37,8 @@ self.addEventListener("fetch", event => {
 
   event.respondWith(
 
-    caches.match(event.request)
-      .then(response => {
-
-        return response || fetch(event.request);
-
-      })
+    fetch(event.request)
+      .catch(() => caches.match(event.request))
 
   );
 
