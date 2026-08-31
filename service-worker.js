c2045b3efef29b31c2f4e5ce918ac97d0f2698c5
telkisd-cmd <telkisd@gmail.com>
2026-08-31 13:12:29 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -28,8 +28,28 @@ self.addEventListener("install", event => {
 self.addEventListener("activate", event => {
 
   event.waitUntil(
-    self.clients.claim()
+
+    caches.keys().then(keys => {
+
+      return Promise.all(
+
+        keys.map(key => {
+
+          if(key !== CACHE_NAME){
+
+            return caches.delete(key);
+
+          }
+
+        })
+
+      );
+
+    })
+
   );
+
+  self.clients.claim();
 
 });
 
